2cee492781d436ae7cea46e86a7609e822261c0d
Jeff Reiner <theundeadghost@gmail.com>
2021-03-18 20:18:22 +0100

    Adding first Route

--- index.ts
+++ index.ts
@@ -4,7 +4,20 @@ const app = express();
 
 const PORT = 8080;
 
-app.get("/", (_, res) => res.send("Express + TypeScript Server"));
+app.get("/", async (_, res) => res.send("🧼 Soapbox Minis Server"));
+
+app.get("/trivia/:roomId", async (req, res) => {
+  try {
+    const roomId = req.params.roomId;
+
+    res.send(roomId);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️ [server]: running at https://localhost:${PORT}`);
