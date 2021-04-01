a5aae2727d5128893e0bf7ea0e3ea68f96a7b5c4
decanus <7621705+decanus@users.noreply.github.com>
2021-04-01 23:18:39 +0200

    just experimenting

---
+++ routes/trivia_v2.ts
@@ -0,0 +1,14 @@
+import * as socketio from "socket.io"
+import express from "express";
+import {Socket} from "socket.io";
+
+const app = express();
+
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+io.of("/trivia").on("connection", (socket: Socket) => {
+
+    socket.handshake.query.room
+
+})
