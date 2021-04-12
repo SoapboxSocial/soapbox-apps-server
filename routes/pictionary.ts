eb4b5cc3816426613d502c63682a44d7b284254b
Jeff Reiner <theundeadghost@gmail.com>
2021-04-12 17:47:41 +0200

    Building out the game

--- routes/pictionary.ts
+++ routes/pictionary.ts
@@ -32,12 +32,22 @@ async function deleteGame(roomID: string) {
   games.delete(roomID);
 }
 
-enum SocketEvents {
-  JOIN_GAME = "JOIN_GAME",
-  CLOSE_GAME = "CLOSE_GAME",
+interface PictionaryListenEvents {
+  JOIN_GAME: ({ user }: { user: User }) => void;
+  CLOSE_GAME: () => void;
+  REROLL_WORDS: () => void;
+  SELECT_WORD: ({ word }: { word: string }) => void;
+  GUESS_WORD: ({ guess }: { guess: string }) => void;
 }
 
-export default function pictionary(io: Server) {
+interface PictionaryEmitEvents {
+  WORDS: ({ words }: { words: string[] }) => void;
+  SEND_WORD: ({ word }: { word: string }) => void;
+}
+
+export default function pictionary(
+  io: Server<PictionaryListenEvents, PictionaryEmitEvents>
+) {
   io.on("connection", (socket) => {
     const roomID = socket.handshake.query.roomID as string;
 
@@ -45,18 +55,70 @@ export default function pictionary(io: Server) {
 
     console.log("[connection] new socket connected with id", socketID);
 
-    socket.on(SocketEvents.JOIN_GAME, async ({ user }: { user: User }) => {
-      console.log("[JOIN_GAME] with payload", user);
+    socket.on("JOIN_GAME", async ({ user }: { user: User }) => {
+      try {
+        console.log("[JOIN_GAME]");
 
-      socket.join(roomID);
+        socket.join(roomID);
 
-      const game = await getOrStartGame(roomID);
+        const game = await getOrStartGame(roomID);
 
-      game.addPlayer(socketID, user);
+        game.addPlayer(socketID, user);
+
+        const word = game.getWord();
+
+        if (typeof word === "undefined") {
+          return;
+        }
+
+        socket.emit("SEND_WORD", { word: word });
+      } catch (error) {
+        console.error(error);
+      }
     });
 
-    socket.on(SocketEvents.CLOSE_GAME, async () => {
+    socket.on("REROLL_WORDS", () => {
+      console.log("[REROLL_WORDS]");
+
+      const instance = games.get(roomID);
+
+      if (typeof instance === "undefined") {
+        return;
+      }
+
+      const wordOptions = instance.getWordOptions();
+
+      socket.emit("WORDS", { words: wordOptions });
+    });
+
+    socket.on("SELECT_WORD", ({ word }) => {
+      console.log("[SELECT_WORD]");
+
+      const instance = games.get(roomID);
+
+      if (typeof instance === "undefined") {
+        return;
+      }
+
+      instance.setWord(word);
+
+      io.in(roomID).emit("SEND_WORD", { word });
+    });
+
+    socket.on("GUESS_WORD", ({ guess }) => {
+      console.log("[GUESS_WORD]");
+
+      const instance = games.get(roomID);
+
+      if (typeof instance === "undefined") {
+        return;
+      }
+    });
+
+    socket.on("CLOSE_GAME", async () => {
       console.log("[CLOSE_GAME]");
+
+      io.in(roomID).disconnectSockets(true);
 
       await deleteGame(roomID);
     });
