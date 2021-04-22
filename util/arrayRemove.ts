38a92056f1642f468adc425ffa883719b6905efa
Jeff Reiner <theundeadghost@gmail.com>
2021-04-22 18:30:27 +0200

    Cleanup

--- util/arrayRemove.ts
+++
@@ -1,3 +0,0 @@
-export default function arrayRemove(arr: any[], value: any) {
-  return arr.filter((elem) => elem !== value);
-}
--- util/getRandom.ts
+++
@@ -1,3 +0,0 @@
-export default function getRandom(max: number) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
