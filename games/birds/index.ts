c7733bba31eaa80fdc99d4da935b780a9470315c
Jeff Reiner <theundeadghost@gmail.com>
2021-05-14 17:18:06 +0200

    Revert "Revert "Improving Birds""
    This reverts commit 6bb46f294b23fe86374129d93d91d0ea70467af2.

--- games/birds/index.ts
+++ games/birds/index.ts
@@ -1,13 +1,19 @@
-import { Server } from "socket.io";
+import { Namespace, Server } from "socket.io";
+import { ServerStateEnum } from "./constants";
 import Game from "./game";
+import { PipeTinyObject } from "./pipe";
+import { PlayerTinyObject } from "./player";
 
 const games = new Map<string, Game>();
 
-function getOrCreateGame(roomID: string) {
+function getOrCreateGame(
+  roomID: string,
+  nsp: Namespace<BirdsListenEvents, BirdsEmitEvents>
+) {
   const instance = games.get(roomID);
 
   if (typeof instance === "undefined") {
-    let game = new Game();
+    let game = new Game(roomID, nsp);
 
     game.start();
 
@@ -19,19 +25,49 @@ function getOrCreateGame(roomID: string) {
   return instance;
 }
 
-function deleteGame(roomID: string) {
+async function deleteGame(roomID: string) {
   const instance = games.get(roomID);
 
   if (typeof instance === "undefined") {
     return;
   }
 
-  instance.stop();
+  await instance.stop();
 
   games.delete(roomID);
 }
 
-export default function birds(io: Server) {
+export interface BirdsListenEvents {
+  close_game: () => void;
+  say_hi: (
+    nick: string,
+    floor: number,
+    fn: (gameState: ServerStateEnum, playerId: string) => void
+  ) => void;
+  player_jump: () => void;
+  change_ready_state: (readyState: boolean) => void;
+}
+
+export interface BirdsEmitEvents {
+  player_disconnect: (socketID: string) => void;
+  update_game_state: (newState: ServerStateEnum) => void;
+  game_loop_update: (update: {
+    players: PlayerTinyObject[];
+    pipes: PipeTinyObject[];
+  }) => void;
+  player_ready_state: (player: PlayerTinyObject) => void;
+  player_list: (players: PlayerTinyObject[]) => void;
+  new_player: (player: PlayerTinyObject) => void;
+  ranking: (ranking: {
+    score: number;
+    bestScore: number;
+    rank: number;
+    nbPlayers: number;
+    highscores: { player: string; score: number }[];
+  }) => void;
+}
+
+export default function birds(io: Server<BirdsListenEvents, BirdsEmitEvents>) {
   const nsp = io.of("/birds");
 
   nsp.on("connection", (socket) => {
@@ -45,22 +81,44 @@ export default function birds(io: Server) {
       socketID
     );
 
-    if (roomID === "" || roomID === undefined) {
-      socket.disconnect();
+    const game = getOrCreateGame(roomID, nsp);
 
-      return;
-    }
+    game.playersManager.addNewPlayer(socket, socketID);
 
-    socket.on("close_game", () => {
+    socket.on("say_hi", (nick, floor, fn) => {
+      fn(game.state, socketID);
+
+      game.playerLog(socket, nick, floor);
+    });
+
+    socket.on("disconnect", (reason) => {
+      console.log(
+        "[birds]",
+        "[disconnect] socket disconnected with reason",
+        reason
+      );
+
+      const game = games.get(roomID);
+
+      if (typeof game === "undefined") {
+        return;
+      }
+
+      game.playersManager.removePlayer(socketID);
+
+      socket.to(roomID).emit("player_disconnect", socketID);
+    });
+
+    socket.on("close_game", async () => {
       console.log(
         "[birds]",
         "[close_game]",
         `deleting game with roomID of ${roomID}`
       );
 
-      deleteGame(roomID);
-    });
+      await deleteGame(roomID);
 
-    getOrCreateGame(roomID).handle(socket);
+      nsp.in(roomID).disconnectSockets();
+    });
   });
 }
